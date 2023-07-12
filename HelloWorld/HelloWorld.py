6746cc2b66365eb37f9f693a10329d782ded1c6c
Diego H. Laptop <hola@mrdiego.dev>
2023-07-12 10:24:25 -0600

    Hello World

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,15 @@
+#program Hello World! 
+print('Hello World!')
+
+## Printing using single and double quotes
+print('Diego')
+print("Diego")
+
+#Printing variables
+my_name = 'Diego'
+print(my_name)
+
+#integers and floats
+
+myAge = 26
+myHeight = 1.68
